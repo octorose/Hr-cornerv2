da1091b084d44a59fc5aa5d46298820f48f74da0
ocotorse <allouchhatim@gmail.com>
2024-05-09 10:08:43 +0100

    feat: Id generator and a Stream to String Converter

---
+++ package/functions/PendingCandidatIdGenerator.ts
@@ -0,0 +1,13 @@
+import KSUID from "ksuid";
+
+export const generateId = () => {
+  const id: any = Math.floor(Math.random() * 99999999999);
+  console.log(id.toString());
+  
+  return id.totring;
+};
+export const CandidatIdGenerator = () => {
+  const id = "Pend_hst";
+
+  return id;
+};
---
+++ package/functions/StreamtoString.ts
@@ -0,0 +1,7 @@
+export async function streamToString(stream: any) {
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks).toString("ascii");
+}
